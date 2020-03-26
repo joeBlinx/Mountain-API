d3f85ae9caaac792e003c076589bd27d640a0a89
joeBlinx <aiglestiven@gmail.com>
2020-03-26 09:32:26 +0100

    build again

---
+++ build/CMakeFiles/sandBox.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/SandBox/sandbox_useful/basicInit.cpp" "/home/user/SandBox/build/CMakeFiles/sandBox.dir/sandbox_useful/basicInit.cpp.o"
+  "/home/user/SandBox/sandbox_useful/device.cpp" "/home/user/SandBox/build/CMakeFiles/sandBox.dir/sandbox_useful/device.cpp.o"
+  "/home/user/SandBox/sandbox_useful/renderpass/renderPass.cpp" "/home/user/SandBox/build/CMakeFiles/sandBox.dir/sandbox_useful/renderpass/renderPass.cpp.o"
+  "/home/user/SandBox/sandbox_useful/swapChain.cpp" "/home/user/SandBox/build/CMakeFiles/sandBox.dir/sandbox_useful/swapChain.cpp.o"
+  "/home/user/SandBox/src/initVulkan.cpp" "/home/user/SandBox/build/CMakeFiles/sandBox.dir/src/initVulkan.cpp.o"
+  "/home/user/SandBox/src/log.cpp" "/home/user/SandBox/build/CMakeFiles/sandBox.dir/src/log.cpp.o"
+  "/home/user/SandBox/src/main.cpp" "/home/user/SandBox/build/CMakeFiles/sandBox.dir/src/main.cpp.o"
+  "/home/user/SandBox/src/utils.cpp" "/home/user/SandBox/build/CMakeFiles/sandBox.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_VULKAN"
+  "LZMA_API_STATIC"
+  "XML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/user/.conan/data/glfw/3.3.2/bincrafters/stable/package/6a0b195b06ef5fbc1ca4422976fe500aeb5bab2c/include"
+  "/home/user/.conan/data/libx11/1.6.8/bincrafters/stable/package/fb4de4452309896753239ba8144a4acb9d0f0783/include"
+  "/home/user/.conan/data/xorgproto/2019.1/bincrafters/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/user/.conan/data/xtrans/1.4.0/bincrafters/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/user/.conan/data/libxcb/1.13.1/bincrafters/stable/package/b8c98b327e58b392e50971217071e1166220829f/include"
+  "/home/user/.conan/data/libxau/1.0.9/bincrafters/stable/package/c99c7836a95e8848c6575783d12ace1982abacab/include"
+  "/home/user/.conan/data/libxdmcp/1.1.3/bincrafters/stable/package/27635b69fcb5b0b49e17a4c6b12ffba465651c72/include"
+  "/home/user/.conan/data/xproto/7.0.31/bincrafters/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/user/.conan/data/libxrandr/1.5.2/bincrafters/stable/package/3ed31e8e448b5f476748003b2cb8087256acc7e3/include"
+  "/home/user/.conan/data/libxrender/0.9.10/bincrafters/stable/package/3094f02cc92d072ef64199f75ca7e0f3a45fa967/include"
+  "/home/user/.conan/data/libxext/1.3.4/bincrafters/stable/package/3094f02cc92d072ef64199f75ca7e0f3a45fa967/include"
+  "/home/user/.conan/data/libxinerama/1.1.4/bincrafters/stable/package/24899c9c48c7638e3ffc80c34aaa9ea07b516d56/include"
+  "/home/user/.conan/data/libxfixes/5.0.3/bincrafters/stable/package/3094f02cc92d072ef64199f75ca7e0f3a45fa967/include"
+  "/home/user/.conan/data/libxcursor/1.2.0/bincrafters/stable/package/dac6d457f8e28d09d9c6c27a83eb2b6b5aff3fc7/include"
+  "/home/user/.conan/data/libxi/1.7.10/bincrafters/stable/package/24899c9c48c7638e3ffc80c34aaa9ea07b516d56/include"
+  "/home/user/.conan/data/mesa/19.3.1/bincrafters/stable/package/4738ebe557ba170d04ecce21fcb989f0bce5f442/include"
+  "/home/user/.conan/data/zlib/1.2.11/_/_/package/23b828d52c0630e6b0b96d2945419feb7843c4f8/include"
+  "/home/user/.conan/data/expat/2.2.9/_/_/package/23b828d52c0630e6b0b96d2945419feb7843c4f8/include"
+  "/home/user/.conan/data/libdrm/2.4.100/bincrafters/stable/package/2c29f667cacc7eeb03f56d878f99f7ce8b3d6168/include"
+  "/home/user/.conan/data/libdrm/2.4.100/bincrafters/stable/package/2c29f667cacc7eeb03f56d878f99f7ce8b3d6168/include/libdrm"
+  "/home/user/.conan/data/libdrm/2.4.100/bincrafters/stable/package/2c29f667cacc7eeb03f56d878f99f7ce8b3d6168/include/libkms"
+  "/home/user/.conan/data/libpciaccess/0.16/bincrafters/stable/package/23b828d52c0630e6b0b96d2945419feb7843c4f8/include"
+  "/home/user/.conan/data/libelf/0.8.13/_/_/package/23b828d52c0630e6b0b96d2945419feb7843c4f8/include"
+  "/home/user/.conan/data/libunwind/1.3.1/_/_/package/eef58b252f2cfddca30f2e8758005c7d67542d8a/include"
+  "/home/user/.conan/data/xz_utils/5.2.4/_/_/package/23b828d52c0630e6b0b96d2945419feb7843c4f8/include"
+  "/home/user/.conan/data/libxdamage/1.1.5/bincrafters/stable/package/c1d5e051bbd1076350f63a36fe423230726ed6a6/include"
+  "/home/user/.conan/data/libxshmfence/1.3/bincrafters/stable/package/98bc0352e079a667d0edbedb786305b0971aa660/include"
+  "/home/user/.conan/data/libxxf86vm/1.1.4/bincrafters/stable/package/b305f54ba4e27810ceb49cac130bdec766fea836/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sandBox.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sandBox.dir/sandbox_useful/basicInit.cpp.o"
+  "CMakeFiles/sandBox.dir/sandbox_useful/device.cpp.o"
+  "CMakeFiles/sandBox.dir/sandbox_useful/renderpass/renderPass.cpp.o"
+  "CMakeFiles/sandBox.dir/sandbox_useful/swapChain.cpp.o"
+  "CMakeFiles/sandBox.dir/src/initVulkan.cpp.o"
+  "CMakeFiles/sandBox.dir/src/log.cpp.o"
+  "CMakeFiles/sandBox.dir/src/main.cpp.o"
+  "CMakeFiles/sandBox.dir/src/utils.cpp.o"
+  "bin/sandBox"
+  "bin/sandBox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sandBox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
