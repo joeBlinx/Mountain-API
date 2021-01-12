f0abccafc08d767e0d86551b53c8ef462c964013
joeBlinx <aiglestiven@gmail.com>
2021-01-12 18:16:40 +0100

    the obj model is loading correctly and the texture is loaded well too

--- sandbox_useful/src/load_model.cpp
+++ sandbox_useful/src/load_model.cpp
@@ -31,7 +31,7 @@ namespace model{
                     .color{1.0f, 1.0f, 1.0f},
                     .tex_coord{
                             attrib.texcoords[2 * index.texcoord_index + 0],
-                            attrib.texcoords[2 * index.texcoord_index + 1]
+                            1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
                     }
                 });
             }
