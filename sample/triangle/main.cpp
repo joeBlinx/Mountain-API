7cfa1f984eca9ad6c66be2d6b666e59f91fe92cc
JoeBlinx <aiglestiven@gmail.com>
2021-01-21 13:56:52 +0100

    add some color to triangle sample

--- sample/triangle/main.cpp
+++ sample/triangle/main.cpp
@@ -27,6 +27,7 @@ int main(){
                     height,
                     "Vulkan Triangle",
                     devicesExtension};
+
     using mountain::subpass_attachment;
     mountain::RenderPass render_pass{
             context,
@@ -41,7 +42,8 @@ int main(){
             height
     };
     struct Vertex{
-        glm::vec2 pos;
+        glm::vec2 pos; // location 0
+        glm::vec3 color; // location 1
     };
     /* This is a triangle that will be shown
      *                    1 (0., -0.5f) don't forget that the y axis
@@ -51,9 +53,9 @@ int main(){
      * (-0.25, 0)    2 ______ 3 (0.25, 0.)
      */
     std::vector<Vertex> vertices{
-            Vertex{{0.f, -0.5f}}, // 1
-            Vertex{{-0.25f, 0.f}},// 2
-            Vertex{{0.25f, 0.f}} // 3
+            Vertex{{0.f, -0.5f}, {1.0f, 0.f, 0.f}}, // 1
+            Vertex{{-0.25f, 0.f}, {0.0f, 1.f, 0.f}},// 2
+            Vertex{{0.25f, 0.f}, {0.0f, 0.f, 1.f}} // 3
     };
     std::vector<uint32_t> indices{0, 1, 2};
     std::vector<mountain::buffer::vertex> buffers;
@@ -61,7 +63,7 @@ int main(){
             mountain::buffer::vertex{context,
                                      mountain::buffer::vertex_description(0,
                                                   0,
-                                                  CLASS_DESCRIPTION(Vertex, pos)),
+                                                  CLASS_DESCRIPTION(Vertex, pos, color)),
                       vertices,
                       std::move(indices)}
     );
