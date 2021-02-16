252794269e2ebfdeebeca7be4124b79a8df587a8
joeBlinx <aiglestiven@gmail.com>
2021-02-16 20:19:00 +0100

    install target and cpack zip for windows and tgz for other

---
+++ cmake/utils/package.cmake
@@ -0,0 +1,12 @@
+
+set(CPACK_PACKAGE_CONTACT aiglestiven@gmail.com)
+if(UNIX )
+    set(CPACK_GENERATOR TGZ)
+elseif(WIN32)
+    set(CPACK_GENERATOR ZIP)
+endif()
+set(CPACK_PACKAGE_FILE_NAME ${CMAKE_PROJECT_NAME}-${CMAKE_PROJECT_VERSION})
+include(CPack)
+
+
+
