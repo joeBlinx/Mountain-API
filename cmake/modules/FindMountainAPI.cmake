7ef358a4f62205f14ba7c9f580b4cafecd290f3c
joeBlinx <aiglestiven@gmail.com>
2021-02-16 21:56:45 +0100

    add a warning if mountain api is not found

--- cmake/modules/FindMountainAPI.cmake
+++ cmake/modules/FindMountainAPI.cmake
@@ -3,15 +3,18 @@ find_library(
         NAME MountainAPI
         PATHS ${CMAKE_CURRENT_LIST_DIR}/bin
 )
+if(${_mountainapi_bin} NOT STREQUAL "_mountainapi_bin-NOTFOUND")
+    add_library(MountainAPI SHARED IMPORTED GLOBAL)
+    add_library(Mountain::API ALIAS MountainAPI)
 
-add_library(MountainAPI SHARED IMPORTED GLOBAL)
-add_library(Mountain::API ALIAS MountainAPI)
+    # some header need C++20 features to be build, this value can be overridden by the user
+    target_compile_features(MountainAPI INTERFACE cxx_std_20)
+    target_include_directories(MountainAPI INTERFACE ${CMAKE_CURRENT_LIST_DIR}/include)
 
-# some header need C++20 features to be build, this value can be overridden by the user
-target_compile_features(MountainAPI INTERFACE cxx_std_20)
-target_include_directories(MountainAPI INTERFACE ${CMAKE_CURRENT_LIST_DIR}/include)
+    set_target_properties(MountainAPI PROPERTIES
+            IMPORTED_LOCATION ${_mountainapi_bin}
+            )
 
-set_target_properties(MountainAPI PROPERTIES
-        IMPORTED_LOCATION ${_mountainapi_bin}
-        )
-
+else()
+    message(WARNING "Mountain API not found")
+endif()
