7810f2760d09d01d23ba33f05795bdd248ee5e1b
joeBlinx <aiglestiven@gmail.com>
2021-03-02 19:59:43 +0100

    sub renderpass are working

--- sample/subrenderpass/src/main.cpp
+++ sample/subrenderpass/src/main.cpp
@@ -22,8 +22,10 @@ int main() {
     mountain::Context const context{window, constant::devicesExtension};
 
     mountain::RenderPass const render_pass{context,
-                                           {mountain::RenderPass::COLOR | mountain::RenderPass::DEPTH},
-                                           {first_subpass()}
+                                           {mountain::RenderPass::COLOR | mountain::RenderPass::DEPTH,
+                                            mountain::RenderPass::COLOR | mountain::RenderPass::DEPTH},
+                                           {first_subpass(),
+                                            first_subpass()}
     };
 
     mountain::SwapChain const swap_chain(context,
@@ -57,6 +59,30 @@ int main() {
             .create_pipeline_layout({}, vertex_push_constant)
             .build();
 
+    auto const depth_stencil2 = [] {
+        vk::PipelineDepthStencilStateCreateInfo info{};
+        info.depthTestEnable = VK_FALSE;
+        info.depthWriteEnable = VK_FALSE;
+        info.depthCompareOp = vk::CompareOp::eLess;
+        info.stencilTestEnable = VK_FALSE;
+        return info;
+    }();
+    mountain::GraphicsPipeline front_pipeline = mountain::PipelineBuilder(context)
+            .create_color_blend_state()
+            .create_mutlisampling()
+            .create_rasterizer(vk::PolygonMode::eFill)
+            .create_assembly(vk::PrimitiveTopology::eTriangleList)
+            .create_viewport_info(swap_chain.get_swap_chain_extent())
+            .create_vertex_info(quad_vertex_buffer)
+            .create_depth_stencil_state(depth_stencil2)
+            .define_subpass(mountain::SubPass{&render_pass, 1})
+            .create_shaders_info(std::vector{
+                    mountain::shader{SHADER_FOLDER / "portraitvert.spv", vk::ShaderStageFlagBits::eVertex},
+                    mountain::shader{SHADER_FOLDER / "portraitfrag.spv", vk::ShaderStageFlagBits::eFragment}
+            })
+            .create_pipeline_layout({}, vertex_push_constant)
+            .build();
+
     mountain::CommandBuffer command_buffer(context, swap_chain, render_pass);
     command_buffer.record([&](mountain::CommandBuffer const &command_buffer, std::size_t const index) {
         vk::DeviceSize const offset(0);
@@ -66,11 +92,18 @@ int main() {
         command.bindIndexBuffer(quad_vertex_buffer.get_buffer(), quad_vertex_buffer.get_indices_offset(),
                                 vk::IndexType::eUint32);
         auto const& push_vertex = back_pipeline.get_push_constant(vk::ShaderStageFlagBits::eVertex);
-        glm::mat4 model = glm::scale(glm::translate(glm::mat4{1.}, glm::vec3{0., 0, .5}), glm::vec3{0.5f});
+        glm::mat4 model = glm::translate(glm::mat4{1.}, glm::vec3{0., 0, 0.3});
         command.pushConstants(back_pipeline.get_pipeline_layout(), push_vertex.stageFlags,
                               push_vertex.offset, push_vertex.size, reinterpret_cast<std::byte const*>(&model));
         command.drawIndexed(quad_vertex_buffer.get_indices_count(), 1, 0, 0, 0);
         model = glm::translate(glm::mat4{1.}, glm::vec3{0., 0, 0.4});
+        model = glm::scale(glm::translate(glm::mat4{1.}, glm::vec3{0., 0, .5}), glm::vec3{0.5f});
+
+        command.nextSubpass(vk::SubpassContents::eInline);
+        command.bindPipeline(vk::PipelineBindPoint::eGraphics, front_pipeline.get_pipeline());
+        command.bindVertexBuffers(0, 1, &quad_vertex_buffer.get_buffer(), &offset);
+        command.bindIndexBuffer(quad_vertex_buffer.get_buffer(), quad_vertex_buffer.get_indices_offset(),
+                                vk::IndexType::eUint32);
         command.pushConstants(back_pipeline.get_pipeline_layout(), push_vertex.stageFlags,
                               push_vertex.offset, push_vertex.size, reinterpret_cast<std::byte const*>(&model));
         command.drawIndexed(quad_vertex_buffer.get_indices_count(), 1, 0, 0, 0);
