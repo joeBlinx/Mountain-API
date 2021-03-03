da1a7ccb074f104453ebf1edd64245078804a1cb
joeBlinx <aiglestiven@gmail.com>
2021-03-03 08:16:37 +0100

    add structure CommandBufferRecord

--- sample/subrenderpass/src/init.cpp
+++ sample/subrenderpass/src/init.cpp
@@ -2,6 +2,7 @@
 // Created by stiven on 3/2/21.
 //
 
+#include <glm/gtx/transform.hpp>
 #include "init.h"
 namespace color{
     glm::vec4 constexpr RED {1., 0, 0, 1.};
@@ -33,4 +34,29 @@ mountain::buffer::vertex create_quad_vertex_buffer(mountain::Context const& cont
             vertex,
             std::array{0u, 1u, 2u, 1u, 2u, 3u}
     };
+}
+
+void CommandBufferRecorded::operator()(const mountain::CommandBuffer &command_buffer, const std::size_t index) {
+    vk::DeviceSize const offset(0);
+    auto const &command = command_buffer.get_command_buffer(index);
+    command.bindPipeline(vk::PipelineBindPoint::eGraphics, back_pipeline.get_pipeline());
+    command.bindVertexBuffers(0, 1, &quad_vertex_buffer.get_buffer(), &offset);
+    command.bindIndexBuffer(quad_vertex_buffer.get_buffer(), quad_vertex_buffer.get_indices_offset(),
+                            vk::IndexType::eUint32);
+    auto const& push_vertex = back_pipeline.get_push_constant(vk::ShaderStageFlagBits::eVertex);
+    glm::mat4 model = glm::translate(glm::mat4{1.}, glm::vec3{0., 0, 0.3});
+    command.pushConstants(back_pipeline.get_pipeline_layout(), push_vertex.stageFlags,
+                          push_vertex.offset, push_vertex.size, reinterpret_cast<std::byte const*>(&model));
+    command.drawIndexed(quad_vertex_buffer.get_indices_count(), 1, 0, 0, 0);
+    model = glm::translate(glm::mat4{1.}, glm::vec3{0., 0, 0.4});
+    model = glm::scale(glm::translate(glm::mat4{1.}, glm::vec3{0., 0, .5}), glm::vec3{0.5f});
+
+    command.nextSubpass(vk::SubpassContents::eInline);
+    command.bindPipeline(vk::PipelineBindPoint::eGraphics, front_pipeline.get_pipeline());
+    command.bindVertexBuffers(0, 1, &quad_vertex_buffer.get_buffer(), &offset);
+    command.bindIndexBuffer(quad_vertex_buffer.get_buffer(), quad_vertex_buffer.get_indices_offset(),
+                            vk::IndexType::eUint32);
+    command.pushConstants(front_pipeline.get_pipeline_layout(), push_vertex.stageFlags,
+                          push_vertex.offset, push_vertex.size, reinterpret_cast<std::byte const*>(&model));
+    command.drawIndexed(quad_vertex_buffer.get_indices_count(), 1, 0, 0, 0);
 }
--- sample/subrenderpass/src/init.h
+++ sample/subrenderpass/src/init.h
@@ -6,6 +6,7 @@
 #define MOUNTAIN_API_INIT_H
 #include <vulkan/vulkan.hpp>
 #include <glm/glm.hpp>
+#include <mountain/command_buffer.h>
 #include "mountain/vertex.h"
 
 vk::SubpassDependency first_subpass();
@@ -18,5 +19,13 @@ struct VertexWithColor{
     }
 };
 
+struct CommandBufferRecorded{
+    void operator()(mountain::CommandBuffer const& command_buffer, std::size_t const index);
+    mountain::GraphicsPipeline const& back_pipeline;
+    mountain::GraphicsPipeline const& front_pipeline;
+    mountain::buffer::vertex const& quad_vertex_buffer;
+
+
+};
 mountain::buffer::vertex create_quad_vertex_buffer(mountain::Context const& context);
 #endif //MOUNTAIN_API_INIT_H
