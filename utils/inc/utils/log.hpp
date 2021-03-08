4cdeaff7601ee954a88cd731581c49f3e9379108
joeBlinx <aiglestiven@gmail.com>
2021-03-08 11:16:33 +0100

    add symbole into .so

--- utils/inc/utils/log.hpp
+++ utils/inc/utils/log.hpp
@@ -9,9 +9,10 @@
 #include <iosfwd>
 #include <string>
 #include <map>
+#include "mountain/mountainapi_export.h"
 extern std::map<int, std::string> VkResultStr;
 
-void checkError(VkResult result,  std::string const& log);
+MOUNTAINAPI_EXPORT void checkError(VkResult result,  std::string const& log);
 inline void checkError(vk::Result result,  std::string const& log){
     checkError(static_cast<VkResult>(result), log);
 }
